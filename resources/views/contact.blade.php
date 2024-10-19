404eb109376c41c6a1997faa5a92cf1d8d699d30
roblebob <robert.koppisch@gmail.com>
2024-10-19 17:32:38 +0200

    day 4 DONE

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Page</title>
-</head>
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
     <h1>Hello from the Contact Page</h1>
-</body>
-</html>
+</x-layout>
