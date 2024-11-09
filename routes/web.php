141106b3c31b7508d5fc4b0a12700dff732b4da6
roblebob <robert.koppisch@gmail.com>
2024-11-09 18:51:21 +0100

    day 19: Forms: Routes Reloaded - 6 Essential Tips  DONE

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,20 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {return view('home');});
 Route::view('/', 'home');
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job:id}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+//Route::controller(JobController::class)->group( function () {
+//    Route::get('/jobs', 'index');
+//    Route::get('/jobs/create', 'create');
+//    Route::get('/jobs/{job:id}', 'show');
+//    Route::post('/jobs', 'store');
+//    Route::get('/jobs/{job}/edit', 'edit');
+//    Route::patch('/jobs/{job}', 'update');
+//    Route::delete('/jobs/{job}', 'destroy');
+//});
+
+Route::resource('jobs', JobController::class);
+//Route::resource('jobs', JobController::class, ['only' => ['index', 'show']]);
+//Route::resource('jobs', JobController::class, ['except' => ['index', 'show']]);
+
 
 
 Route::view('/contact', 'contact');
