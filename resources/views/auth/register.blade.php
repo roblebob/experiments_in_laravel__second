f1b18411c64887702edd1c08041cc1c3f513b20b
roblebob <robert.koppisch@gmail.com>
2024-11-10 12:05:42 +0100

    Make a Login and Registration from Scratch: Part 2  DONE

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
                     <x-form-field>
                         <x-form-label for="password_confirmation">Confirm Password</x-form-label>
                         <div class="mt-2">
-                            <x-form-input name="password_confirmation" id="password_confirmation" type="password_confirmation" required ></x-form-input>
+                            <x-form-input name="password_confirmation" id="password_confirmation" type="password" required ></x-form-input>
                             <x-form-error name="password_confirmation"/>
                         </div>
                     </x-form-field>
